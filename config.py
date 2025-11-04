c70b1c7f7f5c66153a698adf2bafa3fa65d85c21
mrstrangerpvt2024-cmyk <mrstrangerpvt2024@gmail.com>
2025-11-04 11:49:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    VIP_USERS = [int(x) for x in os.environ.get("VIP_USERS", "").split()]
+    VIP_USERS = [int(x) for x in os.environ.get("VIP_USERS", "7678862761").split()]
